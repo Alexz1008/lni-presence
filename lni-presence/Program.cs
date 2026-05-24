1d228f65c092a047bc4f4221bf27a2bcc42048cf
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2026-05-23 21:30:54 -0700

    Add debug path for local test

--- lni-presence/Program.cs
+++ lni-presence/Program.cs
@@ -11,8 +11,25 @@ var host = new HostBuilder()
     {
         services.AddSingleton(_ =>
         {
-            var credential = new DefaultAzureCredential();
-            return new GraphServiceClient(credential, ["https://graph.microsoft.com/.default"]);
+            var debugMode = context.Configuration["DEBUG_SELF_PRESENCE"];
+            if (string.Equals(debugMode, "true", StringComparison.OrdinalIgnoreCase))
+            {
+                var tenantId = context.Configuration["AZURE_TENANT_ID"];
+                var clientId = context.Configuration["AZURE_CLIENT_ID"];
+                var credential = new InteractiveBrowserCredential(
+                    new InteractiveBrowserCredentialOptions
+                    {
+                        TenantId = tenantId,
+                        ClientId = clientId,
+                        RedirectUri = new Uri("http://localhost:1234")
+                    });
+                return new GraphServiceClient(credential, ["Presence.Read", "User.Read"]);
+            }
+            else
+            {
+                var credential = new DefaultAzureCredential();
+                return new GraphServiceClient(credential, ["https://graph.microsoft.com/.default"]);
+            }
         });
 
         services.AddSingleton(_ =>
